9f0db4317334d3e7f00ade343b79d49ac06c6f23
Jimmy <jimmylanbcn@gmail.com>
2021-03-03 22:09:57 -0500

    :hammer: Add server setup script

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-03-03
+ */
+
+import express from "express";
+
+const app = express();
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const express = require("express");
-const app = express();
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-03-03
+ */
 
+import { config } from "dotenv";
+import errorhandler from "errorhandler";
+
+import { app } from "./app";
+
+// Environmental variables
+// Remove this line if the variables are not loaded from
+// dotenv files.
+config();
+
+// Server configurations
 const PORT = process.env.PORT || 3000;
+const ENVIRONMENT = process.env.NODE_ENV || app.get("env");
+const SERVER_NAME = process.env.SERVER_NAME || "Server";
+
+// Provide full stack trace
+if (ENVIRONMENT === "development") {
+  app.use(errorhandler);
+}
+
+// Start server
 app.listen(PORT, () => {
-  console.log(app.get("port"));
+  console.group(`[${SERVER_NAME}] Server started successfully.`);
+  console.info(`PORT: ${PORT}`);
+  console.info(`ENV: ${ENVIRONMENT}`);
+  console.info(`TIME: ${new Date()}`);
+  console.groupEnd();
 });
