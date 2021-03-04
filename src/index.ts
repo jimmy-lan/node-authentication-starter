bd6a5574b7d081b96916ed776bc0753c2ce6ded8
Jimmy <jimmylanbcn@gmail.com>
2021-03-04 18:12:43 -0500

    :art: Wrap server setup code in a function

--- src/index.ts
+++ src/index.ts
@@ -13,20 +13,24 @@ config();
 import "./devsupport/console";
 import { app } from "./app";
 
-// Server configuration
-const PORT = app.get("port");
-const ENVIRONMENT = app.get("env");
+const start = () => {
+  // Server configuration
+  const PORT = app.get("port");
+  const ENVIRONMENT = app.get("env");
 
-// Provide full stack trace
-if (ENVIRONMENT === "development") {
-  app.use(errorhandler);
-}
+  // Provide full stack trace
+  if (ENVIRONMENT === "development") {
+    app.use(errorhandler);
+  }
 
-// Start server
-app.listen(PORT, () => {
-  console.group("Server started successfully.");
-  console.info(`PORT: ${PORT}`);
-  console.info(`ENV: ${ENVIRONMENT}`);
-  console.info(`TIME: ${new Date()}\n`);
-  console.groupEnd();
-});
+  // Start server
+  app.listen(PORT, () => {
+    console.group("Server started successfully.");
+    console.info(`PORT: ${PORT}`);
+    console.info(`ENV: ${ENVIRONMENT}`);
+    console.info(`TIME: ${new Date()}\n`);
+    console.groupEnd();
+  });
+};
+
+start();
