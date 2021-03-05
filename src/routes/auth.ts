0247421aa8e3439b12d3884e2491c8ecf0793176
Jimmy <jimmylanbcn@gmail.com>
2021-03-04 19:02:06 -0500

    :sparkles: Add signup request validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,19 +5,31 @@
  */
 import { Router, Request, Response } from "express";
 import { User } from "../models";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares";
 
 const router = Router();
 
-router.post("/signup", async (req: Request, res: Response) => {
-  const { email, password, firstName, lastName } = req.body;
+router.post(
+  "/signup",
+  [
+    body("email").isEmail().isLength({ min: 6, max: 80 }),
+    body("password").isStrongPassword({ minLength: 6 }),
+    body("firstName").isLength({ min: 2, max: 50 }),
+    body("lastName").isLength({ min: 2, max: 50 }),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password, firstName, lastName } = req.body;
 
-  const user = User.build({
-    email,
-    password,
-    profile: { name: { first: firstName, last: lastName } },
-  });
+    const user = User.build({
+      email,
+      password,
+      profile: { name: { first: firstName, last: lastName } },
+    });
 
-  return res.json({ received: true });
-});
+    return res.send({ received: true });
+  }
+);
 
 export { router as authRouter };
