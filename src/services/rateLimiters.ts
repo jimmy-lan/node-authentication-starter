04cede045bc3794cef03cd60d76460830a13dd37
Jimmy <jimmylanbcn@gmail.com>
2021-03-12 23:35:14 -0500

    :lock: Add bruteforce prevention rate limiter

--- src/services/rateLimiters.ts
+++ src/services/rateLimiters.ts
@@ -63,3 +63,41 @@ export const ipRateLimiter = new BurstyRateLimiter(
   ipRateLimiterNormal,
   ipRateLimiterBurst
 );
+
+/* ************************************************
+ * * Password reset attempts rate limiter
+ ************************************************ */
+
+/**
+ * This rate limiter is used for ip + email pair.
+ * This rate limiter should be used as a complement of the
+ * `authBruteIPRateLimiter`.
+ */
+export const passwordResetRateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "password_reset",
+  points: 1,
+  duration: 2 * 60,
+});
+
+/* ************************************************
+ * * Authentication brute force prevention limiter
+ ************************************************ */
+
+/**
+ * This rate limiter is based purely on the ip address.
+ */
+export const authBruteIPRateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "auth_brute_ip",
+  points: 30,
+  duration: 24 * 60 * 60,
+});
+
+export const authBruteEmailAndIPRateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "auth_brute_ip_and_email",
+  points: 6,
+  duration: 30 * 24 * 60 * 60, // Store failed records for 1 month
+  blockDuration: 2 * 60 * 60, // Block for 2 hours after 6 failed attempts
+});
