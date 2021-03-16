30ff270a91732878b5dffe45adfd796b2d23e39e
Jimmy <jimmylanbcn@gmail.com>
2021-03-16 17:59:06 -0400

    :white_check_mark: Add bad request test

--- test/auth/signup.test.ts
+++ test/auth/signup.test.ts
@@ -3,7 +3,14 @@
  * Creation Date: 2021-03-16
  */
 
-import { connectMongo, setEnvVariables, tearDownMongo } from "../common";
+import {
+  apiLink,
+  connectMongo,
+  setEnvVariables,
+  tearDownMongo,
+} from "../common";
+import request from "supertest";
+import { app } from "../../src/app";
 
 describe("sign up api", () => {
   beforeAll(async () => {
@@ -14,5 +21,32 @@ describe("sign up api", () => {
   afterAll(async () => {
     await tearDownMongo();
     jest.clearAllMocks();
+  });
+
+  it("responds with 400 when the request contains invalid fields", async () => {
+    let response;
+
+    response = await request(app).post(apiLink("/signup")).send({}).expect(400);
+
+    expect(response.body.success).toBeDefined();
+    expect(response.body.success).toBeFalsy();
+
+    // Lacking first and last name
+    response = await request(app)
+      .post(apiLink("/signup"))
+      .send({ email: "a@b.com", password: "weoqrle" })
+      .expect(400);
+
+    expect(response.body.success).toBeDefined();
+    expect(response.body.success).toBeFalsy();
+
+    // Lacking password
+    response = await request(app)
+      .post(apiLink("/signup"))
+      .send({ email: "a@b.com", firstName: "Jimmy", lastName: "Lan" })
+      .expect(400);
+
+    expect(response.body.success).toBeDefined();
+    expect(response.body.success).toBeFalsy();
   });
 });
