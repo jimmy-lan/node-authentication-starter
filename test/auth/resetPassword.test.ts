8c89d7967f78183d3a71134ad0e0aa0e5b713219
Jimmy <jimmylanbcn@gmail.com>
2021-03-17 17:21:24 -0400

    Add resetPassword.test.ts

---
+++ test/auth/resetPassword.test.ts
@@ -0,0 +1,8 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-03-17
+ */
+
+jest.mock("../../src/services/redisClient");
+jest.mock("../../src/services/rateLimiters");
+jest.mock("../../src/services/EmailSender");
