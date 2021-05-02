6c033adf3d1837f7f27a1ce49cd04dbca2dc94fa
Jimmy <jimmylanbcn@gmail.com>
2021-05-02 15:09:58 -0400

    :sparkles: Implement get current user route

--- src/routes/auth/currentUser.ts
+++ src/routes/auth/currentUser.ts
@@ -4,11 +4,14 @@
  * Description:
  *     Route to obtain the current signed in user.
  *     If no authorization header is present or the authorization header provided
- *     is invalid, a forbidden error will be thrown.
+ *     is invalid, an unauthorized error will be thrown.
  */
 
 import { Router, Request, Response } from "express";
 import { ResBody, UserRole } from "../../types";
+import { requireAuth } from "../../middlewares";
+import { User } from "../../models";
+import { UnauthorizedError } from "../../errors";
 
 /**
  * Response body structure for the get current user route.
@@ -29,6 +32,42 @@ export interface CurrentUserResBody extends ResBody {
 
 const router = Router();
 
-router.get("/current", (req: Request, res: Response<CurrentUserResBody>) => {});
+router.get(
+  "/current",
+  requireAuth,
+  async (req: Request, res: Response<CurrentUserResBody>) => {
+    const authUser = req.user!;
 
-export { router };
+    const user = await User.findById(authUser.id).lean();
+
+    if (!user) {
+      throw new UnauthorizedError();
+    }
+
+    const {
+      email,
+      profile: {
+        name: { first, last },
+        avatar,
+      },
+      role,
+    } = user;
+
+    return res.json({
+      success: true,
+      payload: {
+        email,
+        role,
+        profile: {
+          name: {
+            first,
+            last,
+          },
+          avatar,
+        },
+      },
+    });
+  }
+);
+
+export { router as currentUserRoute };
