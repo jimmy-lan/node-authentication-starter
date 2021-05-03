d4fe9498f230a061ac576c49fd2cce45379b9a80
Jimmy <jimmylanbcn@gmail.com>
2021-05-02 21:32:57 -0400

    :white_check_mark: Add currentUser.test.ts

---
+++ test/auth/currentUser.test.ts
@@ -0,0 +1,71 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-05-02
+ */
+
+import { app } from "../../src/app";
+import {
+  connectMongo,
+  setEnvVariables,
+  tearDownMongo,
+  apiLink,
+} from "../common";
+
+import mongoose from "mongoose";
+import request from "supertest";
+import { PasswordEncoder } from "../../src/services";
+import { UserRole } from "../../src/types";
+import { signTokens } from "../../src/util";
+
+jest.mock("../../src/services/redisClient");
+jest.mock("../../src/services/rateLimiters");
+
+describe("current user api", () => {
+  const sampleUser = {
+    email: "user@thepolyteam.com",
+    password: "password",
+    clientSecret: PasswordEncoder.randomString(20),
+    role: UserRole.member,
+    // Not in database
+    accessToken: "", // to be updated in beforeAll
+  };
+
+  beforeAll(async () => {
+    setEnvVariables();
+    await connectMongo();
+
+    // Setup sample user
+    const { email, password, clientSecret, role } = sampleUser;
+    const sampleUserEntry = {
+      email,
+      password: await PasswordEncoder.toHash(password),
+      clientSecret,
+      role,
+    };
+
+    // Insert to document
+    const userCollection = mongoose.connection.collection("users");
+    await userCollection.insertOne(sampleUserEntry, {});
+
+    // Generate access token for sample user
+    const userDocument = await userCollection.findOne({ email });
+    // WARNING: Assume `signTokens` works correctly
+    const [_, accessToken] = await signTokens(userDocument, false);
+    sampleUser.accessToken = accessToken;
+  });
+
+  afterAll(async () => {
+    jest.clearAllMocks();
+    await tearDownMongo();
+  });
+
+  it("responds with 401 if authorization header is not present.", async () => {
+    const response = await request(app)
+      .get(apiLink("/current"))
+      .send()
+      .expect(401);
+
+    expect(response.body.success).toBeDefined();
+    expect(response.body.success).toBeFalsy();
+  });
+});
