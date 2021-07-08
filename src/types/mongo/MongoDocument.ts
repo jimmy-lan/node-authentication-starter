dd1b2c73d8c28fb3618736adb442f3178fec56ba
Jimmy <jimmylanbcn@gmail.com>
2021-07-07 21:00:00 -0400

    :sparkles: Add timestamp property to mongo document

--- src/types/mongo/MongoDocument.ts
+++ src/types/mongo/MongoDocument.ts
@@ -4,9 +4,10 @@
 
 import { Document } from "mongoose";
 import { DeepRequired } from "../general";
+import { Timestamp } from "./Timestamp";
 
 /**
  * A type for mongodb document with its document structure
  * defined as `T`.
  */
-export type MongoDocument<T> = Document<unknown> & DeepRequired<T>;
+export type MongoDocument<T> = Document<unknown> & DeepRequired<T> & Timestamp;
